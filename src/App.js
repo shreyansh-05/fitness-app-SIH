0dde1ae4cdaeba18082bda0824b491cd66239839
Shreyansh Shukla <rshreyansh532@gmail.com>
2023-09-05 17:10:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route} from "react-router-dom";
+import {Box} from "@mui/material";
+
 import './App.css';
+
+import Home from "./pages/Home"
+import ExerciesDetail from "./pages/ExerciesDetail"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box width="400px" sx={{ width: {xl: "1488px"}}} m="auto">
+      <Navbar />
+      <Routes >
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciesDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
